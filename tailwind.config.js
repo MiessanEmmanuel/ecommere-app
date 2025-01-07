29304f1ffc57940d58d5504247bd6d2d60730fa3
MiessanEmmanuel <emmanueljeanmiessan@gamil.com>
2025-01-07 06:08:24 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.tsx',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors:{
+                primary: '#da3743',
+                secondary: '#307f9e',
+                dark: '#2d333f',
+            }
+        },
+    },
+
+    plugins: [forms],
+};
